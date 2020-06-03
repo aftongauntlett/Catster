94b9a266e9e81ea3dd9ee1882554481c4487fa98
Afton Gauntlett <afton.gauntlett@gmail.com>
2020-06-03 15:09:34 -0400

    wip

--- src/components/Shared/Navbar/index.js
+++ src/components/Shared/Navbar/index.js
@@ -2,10 +2,6 @@ import React from "react";
 import {
   Navbar,
   Nav,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button,
 } from "react-bootstrap";
 
 function myNavbar() {
---
+++ src/components/Views/Home/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./styles.css";
+import Jumbotron from 'react-bootstrap/Jumbotron'
+
+
+export default class Jumbotron extends Component {
+  render() {
+    return (
+  
+    );
+  }
+}
